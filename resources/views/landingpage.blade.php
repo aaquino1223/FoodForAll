0b1ba3719934e65a57dcd622678d88f96df259bf
Brunnell <bv601@nyu.edu>
2019-05-09 15:14:14 -0400

    Landing Page
    added back the original code and created a new styling sheet

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <title>Full Screen Landing Page</title>
+    <link rel="stylesheet" type= "text/css" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+<section class="intro">
+    <div class="inner">
+        <a class="login" href="#">Login</a>
+        <div class="content"></div>
+        <h1>FoodLink</h1>
+        <a class="cta" href="#">Get Started</a>
+    </div>
+</section>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+    software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</body>
+</html>
