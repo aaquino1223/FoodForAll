cfb413e202bf6a440faa750f523f4e818ca1c531
Brunnell <bv601@nyu.edu>
2019-05-11 22:35:00 -0400

    Create Post

--- resources/views/component/createpost.blade.php
+++ resources/views/component/createpost.blade.php
@@ -1,35 +1,72 @@
+<form>
 <div class="container">
     <div class="row justify-content-center">
         <div class="card w-50">
             <div class="card-header">
-                <ul class="nav nav-pills mb-3 justify-content-between" id="pills-tab" role="tablist">
+                <ul class="nav nav-pills mb-3 nav-justified" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Donation</a>
+                        <a class="nav-link active" id="pills-donation-tab" data-toggle="pill" href="#pills-donation" role="tab" aria-controls="pills-donation" aria-selected="true">Donation</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Event</a>
+                        <a class="nav-link" id="pills-event-tab" data-toggle="pill" href="#pills-event" role="tab" aria-controls="pills-event" aria-selected="false">Event</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Help</a>
+                        <a class="nav-link" id="pills-help-tab" data-toggle="pill" href="#pills-help" role="tab" aria-controls="pills-help" aria-selected="false">Help</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Other</a>
+                        <a class="nav-link" id="pills-other-tab" data-toggle="pill" href="#pills-other" role="tab" aria-controls="pills-other" aria-selected="false">Other</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                            <div class="card bg-light mb-3" style="max-width: 36rem;">
-                                <div class="card-body">
+                    <div class="tab-pane fade show active" id="pills-donation" role="tabpanel" aria-labelledby="pills-donation-tab">
+                            <div class="card bg-light mb-3" style="max-width: 36rem">
+                                <div class="card-header">Food Item #1
                                     <button type="button" class="close" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
+                                <div class="card-body">
+                                    <form>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="fooditem" aria-describedby="whichfood" placeholder="Which food item are you donating?">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="number" min="1" class="form-control" id="fooditem" aria-describedby="howmany" placeholder="How many?">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="fooditem" aria-describedby="whatmeasure" placeholder="In what measure? e.g. servings, oz, lbs">
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
+                        <div class="row">
+                            <div class="col-8">
+                        <button type="button" class="btn btn-default" aria-label="Add" style="width: 50px; height: 50px">
+                            <img src="{{asset('/assets/adding.svg')}}" class="img-fluid" alt="Add">
+                        </button>
+                            </div>
+                            <div class="col-4 align-self-center">
+                        <button type="button" class="btn btn-link">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Next</button>
+                            </div>
+                        </div>
                     </div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                    <div class="tab-pane fade" id="pills-event" role="tabpanel" aria-labelledby="pills-event-tab">
+                        @component('component.postform')
+                        @endcomponent
+                    </div>
+                    <div class="tab-pane fade" id="pills-help" role="tabpanel" aria-labelledby="pills-help-tab">
+                        @component('component.postform')
+                        @endcomponent
+                    </div>
+                    <div class="tab-pane fade" id="pills-other" role="tabpanel" aria-labelledby="pills-other-tab">
+                        @component('component.postform')
+                        @endcomponent
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</div>
+</form>
---
+++ resources/views/component/postform.blade.php
@@ -0,0 +1,38 @@
+<div class="row justify-content-end">
+    <div class="col-8 align-self-center">
+    <label for="dropdownMenuButton";">Who Can See?</label>
+    </div>
+    <div class="dropdown mb-2 col-4 align-self-center">
+        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Anyone
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Anyone</a>
+            <a class="dropdown-item" href="#">Associate Only</a>
+            <a class="dropdown-item" href="#">Associates & Followers</a>
+        </div>
+    </div>
+</div>
+<div class="form-group">
+    <input type="text" class="form-control" id="exampleTitle" aria-describedby="titleHelp" placeholder="Title/Subject">
+</div>
+<div class="form-group">
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"placeholder="Anything else to add?"></textarea>
+</div>
+<div class="row">
+    <div class="col-8">
+    <button type="button" class="btn btn-default" aria-label="Picture" style="width: 50px; height: 50px">
+        <img src="{{asset('/assets/camera.svg')}}" class="img-fluid" alt="Picture">
+    </button>
+    <button type="button" class="btn btn-default" aria-label="Video" style="width: 50px; height: 50px">
+        <img src="{{asset('/assets/video-camera.svg')}}" class="img-fluid" alt="Video">
+    </button>
+    <button type="button" class="btn btn-default" aria-label="Location" style="width: 50px; height: 50px">
+        <img src="{{asset('/assets/pin.svg')}}" class="img-fluid" alt="Location">
+    </button>
+    </div>
+    <div class="col-4 align-self-center">
+    <button type="button" class="btn btn-link">Cancel</button>
+    <button type="submit" class="btn btn-primary">Post</button>
+    </div>
+</div>
