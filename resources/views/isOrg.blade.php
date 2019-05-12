fdda018476ec9b517582bb8290d435f01356bfd0
Brunnell <bv601@nyu.edu>
2019-05-12 03:47:22 -0400

    isOrg Update
    Redid the isOrg

--- resources/views/isOrg.blade.php
+++ resources/views/isOrg.blade.php
@@ -2,7 +2,34 @@
 
 @section('content')
 
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <div class="text-center h1 mb-4">I want to sign up for my</div>
+    <div class="card-deck" style="width: 90%">
+        <div class="card">
+            <img src="{{asset('assets/user.svg')}}" class="card-img-top align-self-center mt-4" style="max-width: 50%" alt="self">
+            <div class="card-body">
+                <h5 class="card-title">Self</h5>
+                <p class="card-text"> You are a person who struggles with food security or someone who wants to do something about hunger and food waste.</p>
+                <button type="submit" class="btn btn-lg btn-block btn-outline-primary" form="business_select" value="Submit">Select</button>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/briefcase.svg')}}" class="card-img-top align-self-center mt-4" style="max-width: 50%" alt="business">
+            <div class="card-body">
+                <h5 class="card-title">Business</h5>
+                <p class="card-text">You are large as a Fortune-500 corporation and or as small as a local shop. Either way, you can and want to help!</p>
+                <button type="submit" class="btn btn-lg btn-block btn-outline-primary" form="business_select" value="Submit">Select</button>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{asset('assets/heart.svg')}}" class="card-img-top align-self-center mt-4" style="max-width: 50%" alt="nonprofit">
+            <div class="card-body">
+                <h5 class="card-title">Nonprofit</h5>
+                <p class="card-text">You have already been in the frontlines battling this issue and you know that platform will expand your reach.</p>
+                <button type="submit" class="btn btn-lg btn-block btn-outline-primary" form="business_select" value="Submit">Select</button>
+            </div>
+        </div>
+    </div>
+    <!--div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Select Profile</h1>
         <p class="lead">I want to sign up for my</p>
         <div class="container">
@@ -39,5 +66,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div-->
 @endsection
