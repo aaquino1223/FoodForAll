74a98a1902d0ef0d9b5f1f3747c405a3c7ad37fc
Brunnell <bv601@nyu.edu>
2019-05-12 19:38:41 -0400

    Post
    Added Like/Dislike buttons
    Comment

--- resources/views/component/post.blade.php
+++ resources/views/component/post.blade.php
@@ -17,4 +17,31 @@
         <h5 class="card-title ">{{ $post->Title }}</h5>
         <p class="card-text">{{ $post->Message }}</p>
     </div>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col-6 text-center">
+                <button type="button" class="btn btn-default" aria-label="Like" style="width: 50px; height: 50px">
+                    <img src="{{asset('/assets/like.svg')}}" class="img-fluid" alt="like">
+                </button>
+                <button type="button" class="btn btn-default" aria-label="Dislike" style="width: 50px; height: 50px">
+                    <img src="{{asset('/assets/dislike.svg')}}" class="img-fluid" alt="dislike">
+                </button>
+            </div>
+            <div class="col-6 text-center">
+                <button type="button" class="btn btn-default" aria-label="Comment" style="width: 50px; height: 50px">
+                    <img src="{{asset('/assets/comment.svg')}}" class="img-fluid" alt="comment">
+                </button>
+                <Label aria-label="Comment">Comment</Label>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <input class="form-control" type="text" placeholder="Write a comment...">
+        <div class="row">
+            <div class="col-4">
+                <button type="button" class="btn btn-link">Like</button>
+                <button type="button" class="btn btn-link">Dislike</button>
+            </div>
+        </div>
+    </div>
 </div>
